7197ed3d8819057bec6628e25859a29886f30415
Michael Ilyin <michael.s.ilyin@gmail.com>
2019-09-26 09:57:58 +0300

    Fix auth

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,6 @@ const path = `${process.cwd()}/dist/server`;
 const server = require(path);
 
 export const ssr = functions.https.onRequest((req, resp) => {
-  console.info(req.cookies);
+  console.info('Request cookies', req.signedCookies);
   return server.app(req, resp);
 });
